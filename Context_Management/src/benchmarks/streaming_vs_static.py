24c3b8992eb44c54bddff0093d74cc54513941f5
antenna1-fr <antenna1onepog@gmail.com>
2025-11-02 22:50:32 -0600

    streaming test - mem and speed benchmark soon

--- Context_Management/src/benchmarks/streaming_vs_static.py
+++ Context_Management/src/benchmarks/streaming_vs_static.py
@@ -6,5 +6,9 @@ from context_management.filebundle import FileBundle
 csv_paths = [Path("large_data_A.csv"), Path("large_data_B.csv")]
 
 with FileBundle(csv_paths) as handles:
-    print(handles[1].readline())
-    pass
+    for handle in handles:
+        print(f"Processing {handle.name}")
+        batcher = Batcher(iterable=handle, batch_size=1000, logging_period=50)
+        for batch in batcher:
+
+            pass
